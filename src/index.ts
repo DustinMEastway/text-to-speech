1abad787ba4fa013ce552012f8a245f689ceff9b
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2022-11-19 14:01:25 -0500

    improve HTML to text conversion for chapter text

--- src/index.ts
+++ src/index.ts
@@ -32,20 +32,31 @@ interface ChapterConfig extends BookConfig {
   chapterPath: string;
 }
 
+const textReplacements: [RegExp, string][] = [
+  // Remove comments.
+  [/<!--[\s\S]*?-->/g, ''],
+  // Remove opening and closing tags.
+  [/<[\/!]?\??[A-Za-z][^>]*>/g, ''],
+  // Tidy up newline whitespaces.
+  [/\s*\n\s*/g, '\n'],
+  // Double Quote.
+  [/&quot;|&#34;/g, '&'],
+  // Ampersand.
+  [/&amp;|&#38;/g, '&'],
+  // Single Quote.
+  [/&apos;|&#39;/g, '\''],
+  // Less Than.
+  [/&lt;|&#60;/g, '<'],
+  // Greater Than.
+  [/&gt;|&#62;/g, '>'],
+  // Remove other HTML character entities.
+  [/&[a-z];|&#.{0,5};/g, ''],
+];
+
 async function convertChapterToAudio({ chapterPath, outputPath }: ChapterConfig) {
-  const chapterContent = (await readFile(chapterPath)).toString().replace(
-    // remove comments
-    /<!--[\s\S]*?-->/g,
-    ''
-  ).replace(
-    // remove opening and closing tags
-    /<[\/!]?\??[A-Za-z][^>]*>/g,
-    ''
-  ).replace(
-    // tidy up newline whitespaces
-    /\s*\n\s*/g,
-    '\n'
-  );
+  const chapterContent = textReplacements.reduce((content, [search, replacement]) => {
+    return content.replace(search, replacement);
+  }, (await readFile(chapterPath)).toString());
 
   const chapterOputAudioFileName = `${outputPath}.aiff`;
   const chapterOputTextFileName = `${outputPath}.txt`;
